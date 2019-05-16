81d23b88be127985c7e1f6e0778f246fba2ebb88
joaovbalmeida <joaovbalmeida@gmail.com>
2019-05-16 14:14:32 -0300

    domain

--- vitoria/settings.py
+++ vitoria/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#=))c97k6g^y@%=j8iee@r@lkldkw4!np-xjc*@f(jtv025-a2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', 'bananavitoria.com', 'www.bananavitoria.com']
 
 
 # Application definition
