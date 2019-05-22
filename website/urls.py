cef476817820475670c5bf3dcaeed6096213f911
joaovbalmeida <joaovbalmeida@gmail.com>
2019-05-22 03:13:39 -0300

    pages layout done

--- website/urls.py
+++ website/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
+    path('quemsomos', views.AboutView.as_view(), name='about'),
+    path('producao', views.ProductionView.as_view(), name='production'),
+    path('nossafrota', views.FleetView.as_view(), name='fleet'),
+    path('contato', views.ContactView.as_view(), name='contact'),
 ]
--- website/views.py
+++ website/views.py
@@ -15,3 +15,31 @@ class HomePageView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(HomePageView, self).get_context_data(**kwargs)
         return context
+
+class AboutView(TemplateView):
+    template_name = "about.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(AboutView, self).get_context_data(**kwargs)
+        return context
+        
+class ProductionView(TemplateView):
+    template_name = "production.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductionView, self).get_context_data(**kwargs)
+        return context
+
+class FleetView(TemplateView):
+    template_name = "fleet.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(FleetView, self).get_context_data(**kwargs)
+        return context
+
+class ContactView(TemplateView):
+    template_name = "contact.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(ContactView, self).get_context_data(**kwargs)
+        return context
